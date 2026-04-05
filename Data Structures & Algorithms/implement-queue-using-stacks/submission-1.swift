6938fcb3b802bc3384f138be966f912832dc0060
Naela Fauzul Muna <naelafauzulm@gmail.com>
2026-04-05 21:16:27 +0700

    Add: implement-queue-using-stacks - submission-1

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-1.swift
@@ -0,0 +1,33 @@
+class MyQueue {
+
+    private var q1 : [Int] = []
+
+    init() {
+
+    }
+
+    func push(_ x: Int) {
+        q1.append(x)
+    }
+
+    func pop() -> Int {
+        return q1.removeFirst()
+    }
+
+    func peek() -> Int {
+        return q1.first!
+    }
+
+    func empty() -> Bool {
+        return q1.isEmpty
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * let obj = MyQueue()
+ * obj.push(x)
+ * let param_2 = obj.pop()
+ * let param_3 = obj.peek()
+ * let param_4 = obj.empty()
+ */
