d91cd73dabe53b66381e8e5096aea08eb0bbd75e
Naela Fauzul Muna <naelafauzulm@gmail.com>
2026-09-06 17:47:52 +0800

    Add: binary-tree-postorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-1.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func postorderTraversal(_ root: TreeNode?) -> [Int] {
+        var result = [Int]()
+
+        func traverse(_ node: TreeNode?) {
+            guard let node = node else { return }
+            traverse(node.left)
+            traverse(node.right)
+            result.append(node.val)
+        }
+        traverse(root)
+        return result
+    }
+}
